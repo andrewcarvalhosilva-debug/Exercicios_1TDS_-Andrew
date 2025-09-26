9928b2a803410ab839745f1ce4a59366177b5a00
andrewjacob23 <andrew.carvalho.silva@escola.pr.gov.br>
2025-09-26 11:47:01 -0300

    Create Senha.cpp

---
+++ Senha.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int senha = 0;
+    printf("digite uma senha: \n");
+    scanf("%d", &senha);
+    
+    for(int a = 0; a < 10; a++){
+       for(int b = 0; b < 10; b++){
+           for(int c = 0; c < 10; c++){
+               for(int d = 0; d < 10; d++){
+                   printf("valor: %d %d %d %d \n", a,b,c,d);
+                 }
+              } 
+           }
+        }
+     }
